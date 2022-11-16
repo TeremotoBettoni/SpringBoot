35791837b4b3677d4e35c9011459c6c4456ab5cf
TeremotoBettoni <mtbettoni@gmail.com>
2022-11-16 17:21:05 -0300

    ingreso de las api, server y repository de Valoracion en yose

--- yose/src/main/java/cl/yose/web/api/ValoracionApiRestController.java
+++ yose/src/main/java/cl/yose/web/api/ValoracionApiRestController.java
@@ -1,5 +1,22 @@
 package cl.yose.web.api;
 
-public class ValoracionApiRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import cl.yose.web.models.Valoracion;
+import cl.yose.web.services.ValoracionServiceImpl;
+
+@RestController
+public class ValoracionApiRestController {
+	
+	@Autowired
+	private ValoracionServiceImpl valoracionServiceImpl;
+
+	// http://localhost:8080/guardar/valoracion
+	@RequestMapping("/guardar/valoracion")
+	public Valoracion guardarValoracion(@RequestBody Valoracion valoracion) {
+		return valoracionServiceImpl.guardarValoracion(valoracion);
+	}
 }
--- yose/src/main/java/cl/yose/web/models/Valoracion.java
+++ yose/src/main/java/cl/yose/web/models/Valoracion.java
@@ -19,9 +19,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class Valoracion {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer valoracionId;
+	private Long id;
 	
 	private Boolean meGusta;
+	
+	private Long usuarioId;
 	
 	@Column(updatable=false)
 	@DateTimeFormat(pattern="yyyy-MM-dd")
@@ -30,47 +32,37 @@ public class Valoracion {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;	
 
-	public Valoracion() {
-		super();
-	}
 
-	public Valoracion(int valoracionId, Boolean meGusta) {
-		super();
-		this.valoracionId = valoracionId;
-		this.meGusta = meGusta;
+		public Long getId() {
+		return id;
 	}
-
-	public int getValoracionId() {
-		return valoracionId;
+	public void setId(Long id) {
+		this.id = id;
 	}
-
-	public void setValoracionId(int valoracionId) {
-		this.valoracionId = valoracionId;
-	}
-
 	public Boolean getMeGusta() {
 		return meGusta;
 	}
-
 	public void setMeGusta(Boolean meGusta) {
 		this.meGusta = meGusta;
+	}
+	public Long getUsuarioId() {
+		return usuarioId;
+	}
+	public void setUsuarioId(Long usuarioId) {
+		this.usuarioId = usuarioId;
 	}
 	public Date getCreatedAt() {
 		return createdAt;
 	}
-
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
-
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-
 		// atributos de control
 		@PrePersist
 		protected void onCreate(){
@@ -80,11 +72,5 @@ public class Valoracion {
 		protected void onUpdate(){
 			this.updatedAt = new Date();
 			}
-
-		@Override
-		public String toString() {
-			return "Valoracion [valoracionId=" + valoracionId + ", meGusta=" + meGusta + ", createdAt=" + createdAt
-					+ ", updatedAt=" + updatedAt + "]";
-		}
 	
 }
